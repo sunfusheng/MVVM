0ddcaa59ad24623623ca93996f0e212bab8b9268
sunfusheng <sfsheng0322@gmail.com>
2020-04-14 19:58:06 +0800

    添加DataBinding基本使用

--- app/src/main/java/com/sunfusheng/mvvm/architecture/app/ui/databinding/Author.kt
+++
@@ -1,7 +0,0 @@
-package com.sunfusheng.mvvm.architecture.app.ui.databinding
-
-/**
- * @author sunfusheng
- * @since 2020/4/14
- */
-data class Author(val name: String, val gitHubUrl: String)
--- app/src/main/java/com/sunfusheng/mvvm/architecture/app/ui/databinding/BasicDataBindingActivity.kt
+++ app/src/main/java/com/sunfusheng/mvvm/architecture/app/ui/databinding/BasicDataBindingActivity.kt
@@ -1,10 +1,12 @@
 package com.sunfusheng.mvvm.architecture.app.ui.databinding
 
 import android.os.Bundle
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.sunfusheng.mvvm.architecture.app.R
 import com.sunfusheng.mvvm.architecture.app.databinding.ActivityBasicDatabindingBinding
 import com.sunfusheng.mvvm.architecture.base.BaseActivity
+import com.sunfusheng.mvvm.architecture.lifecycle.LifecycleHandler
 
 /**
  * @author sunfusheng
@@ -12,11 +14,20 @@ import com.sunfusheng.mvvm.architecture.base.BaseActivity
  */
 class BasicDataBindingActivity : BaseActivity() {
 
+    private val mLifecycleHandler: LifecycleHandler = LifecycleHandler(this, Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initActionBar(getString(R.string.title_basic_databinding), showHomeAsUp = true)
+
         val binding: ActivityBasicDatabindingBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_basic_databinding)
 
-        binding.author = Author("sunfusheng", "https://github.com/sunfusheng")
+        binding.userInfo = UserInfo("sunfusheng")
+        mLifecycleHandler.postDelayed({
+            binding.userInfo = UserInfo("Hi, my name is sunfusheng.")
+        }, 2000)
     }
 }
+
+data class UserInfo(val name: String, val gitHubUrl: String = "https://github.com/sunfusheng")
