e7b3c4f21828182e610f4bd13079253ac945429f
sunfusheng <sunfusheng1989@126.com>
2022-06-24 14:54:23 +0800

    add koin lib

--- app/src/main/java/com/sunfusheng/mvvm/app/App.kt
+++ app/src/main/java/com/sunfusheng/mvvm/app/App.kt
@@ -2,6 +2,9 @@ package com.sunfusheng.mvvm.app
 
 import android.app.Application
 import com.tencent.mmkv.MMKV
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 /**
  * @author sunfusheng
@@ -9,8 +12,13 @@ import com.tencent.mmkv.MMKV
  */
 class App : Application() {
 
-  override fun onCreate() {
-    super.onCreate()
-    MMKV.initialize(this)
-  }
+    override fun onCreate() {
+        super.onCreate()
+        MMKV.initialize(this)
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
 }
---
+++ app/src/main/java/com/sunfusheng/mvvm/app/Koin.kt
@@ -0,0 +1,11 @@
+package com.sunfusheng.mvvm.app
+
+import org.koin.dsl.module
+
+/**
+ * @author sunfusheng
+ * @since  2022/06/24
+ */
+val appModule = module {
+
+}
